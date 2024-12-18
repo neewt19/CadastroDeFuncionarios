3549f05036b19c4429405b3b0ab22deb9b5ef7f5
neewt19 <newthonaraujo19@gmail.com>
2024-12-18 14:36:46 -0300

    first commit

---
+++ src/test/java/com/newt/CadastroDeFuncionarios/CadastroDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newt.CadastroDeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
